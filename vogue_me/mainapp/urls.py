bdeff4d98a7d2d78daf9c7bd008ab2cbef7a7b7f
Song Jihun <sksmsvlxk@gmail.com>
2025-09-04 19:26:53 +0900

    Feat: 홈화면( 채팅창 ) UI 1차 완료 #54

--- vogue_me/mainapp/urls.py
+++ vogue_me/mainapp/urls.py
@@ -23,7 +23,5 @@ app_name = 'mainapp'
 urlpatterns = [
     # path('', RedirectView.as_view(url='/user/login'), name="index"),
     path(''         , v.index       , name="index"),
-    path('style'    , v.style       , name="style"),
-    path('our_team' , v.our_team    , name="our_team"),
 
 ]
--- vogue_me/mainapp/views.py
+++ vogue_me/mainapp/views.py
@@ -5,10 +5,4 @@ from django.shortcuts import render
 
 @login_required
 def index(request):
-    return HttpResponse("<h1>여기에 채팅 화면 나오기</h1>")
-
-def style(request):
-    return render(request, "app/mainapp/style.html")
-
-def our_team(request):
-    return render(request, "app/mainapp/our_team.html")
+    return render(request, "app/mainapp/index.html")
