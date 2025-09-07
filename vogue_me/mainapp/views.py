d949b59c22b22e8ea4154d2da86f433039156be7
SONG JIHUN <sksmsvlxk@gmail.com>
2025-09-07 13:28:42 +0900

    Update: index 페이지에 AI 정보 수정. 채팅 입력시 해당 내용으로 스크롤되게 수정.

--- vogue_me/mainapp/views.py
+++ vogue_me/mainapp/views.py
@@ -1,15 +1,28 @@
 # Create your views here.
-import json
 
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
 from django.shortcuts import render
+
+from apiapp.models import ChatHistory
+
 
 @login_required
 def index(request):
+    context = {}
     # 기존에 입력한 선호 스타일이 있는지 체크
+    if ChatHistory.objects.filter(user_id=request.user.id).count() == 0:
+        context["newbie"] = True
 
-    return render(request, "app/mainapp/index.html")
+    # 마지막에 대화했던 AI 정보 호출해서 화면에 구성.
+    last_ai = ChatHistory.objects.last().influencer
+    request.session["last_ai_id"] = last_ai.id
+    context["last_ai"] = {
+        "name"  : last_ai.name,
+        "image" : last_ai.profile_img_url,
+        "voice" : last_ai.voice_info,
+    }
+
+    return render(request, "app/mainapp/index.html", context)
 
 def detail(request, id):
     return render(request, "app/mainapp/detail.html")
