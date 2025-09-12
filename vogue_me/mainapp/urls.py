109f991ca59645f8ef152b0d1b9c46ccd2beafc6
YoonYiseo <gemyiseo@gmail.com>
2025-09-12 11:55:08 +0900

    Feat: survey 페이지 레이아웃

--- vogue_me/mainapp/urls.py
+++ vogue_me/mainapp/urls.py
@@ -8,4 +8,7 @@ urlpatterns = [
     path(''                 , v.index , name="index"),
     path('main/'            , v.main  , name="main"),
     path('detail/<int:id>'  , v.detail, name="detail"),
+
+    #########
+    path('survey', v.survey, name="survey"),
 ]
