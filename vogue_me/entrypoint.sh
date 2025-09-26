2819567aa5dc725ca9a9ec1dac034cd74478c098
Song Jihun <sksmsvlxk@gmail.com>
2025-09-26 16:04:42 +0900

    Update: exec gunicorn -c gunicorn.conf.py --timeout 600 명령어에서 timeout 부분에 = 추가. => --timeout=600

--- vogue_me/entrypoint.sh
+++ vogue_me/entrypoint.sh
@@ -8,4 +8,4 @@ echo "Collecting and Uploading static files into S3 bucket."
 python manage.py collectstatic --noinput
 
 echo "Run gunicorn."
-exec gunicorn -c gunicorn.conf.py --timeout 600
+exec gunicorn -c gunicorn.conf.py --timeout=600
